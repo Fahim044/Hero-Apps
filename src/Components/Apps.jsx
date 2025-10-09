168e224ead05b2bcc73fce74a5a9799b43a455a6
Fahim044 <fahiimfaysalleusc044@gmail.com>
2025-10-09 10:18:57 +0600

    added search feature

--- src/Components/Apps.jsx
+++ src/Components/Apps.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, use } from 'react';
+import React, { Suspense, use, useState } from 'react';
 
 import { Search } from 'lucide-react';
 import ShowAppsContainer from './ShowAppsContainer';
@@ -6,6 +6,17 @@ import ShowAppsContainer from './ShowAppsContainer';
 const Apps = ({appsPromise12}) => {
     const allApps=use(appsPromise12);
     // console.log(allApps);
+    const [search,setSearch]=useState('');
+    const handleSearch=(e)=>{
+        e.preventDefault();
+        // console.log(e.target.value);
+        const inputValue=e.target.value;
+        const searchValue=inputValue.trim().toLowerCase();
+        console.log(searchValue);
+        setSearch(searchValue);
+        // console.log(search);
+
+    }
     return (
         <div className='max-w-11/12 mx-auto'>
             <div className='mt-10 mb-6 space-y-5 '>
@@ -15,12 +26,12 @@ const Apps = ({appsPromise12}) => {
                     <h2 className='text-xl'>({allApps.length}) Apps Found</h2>
                     <div className='flex items-center'>
                         <Search />
-                        <input type="text" placeholder={`Search App`} />
+        <input type="text" onChange={handleSearch}  placeholder={`Search App`} />
                     </div>
                 </div>
             </div>
     <Suspense fallback={<div className='flex items-center justify-center'><span className="loading loading-dots loading-xl "></span></div>}>
-          <ShowAppsContainer allApps={allApps}></ShowAppsContainer>
+          <ShowAppsContainer allApps={allApps} searchValue={search}></ShowAppsContainer>
         </Suspense>
         </div>
     );
--- src/Components/ShowAppsContainer.jsx
+++ src/Components/ShowAppsContainer.jsx
@@ -2,13 +2,16 @@ import React  from 'react';
 import ShowApps from './ShowApps';
 
 
-const ShowAppsContainer = ({allApps}) => {
+const ShowAppsContainer = ({allApps,searchValue}) => {
     // console.log(allApps);
-   
+//    console.log(searchValue);
+const filterApps= allApps.filter(app=>app.title.toLowerCase().includes(searchValue));
+console.log(filterApps);
+const dispApps=((searchValue===false)?allApps:filterApps);
     return (
         <div className='max-w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3'>
        {
-        allApps.map(app=><ShowApps key={app.id} app={app}></ShowApps>)
+        dispApps.map(app=><ShowApps key={app.id} app={app}></ShowApps>)
        }
         </div>
     );
